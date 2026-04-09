e26b97ce1043ae238c591f366e633570254da2c3
Dana Le <danale.ca@outlook.com>
2026-04-09 01:28:45 -0400

    Initial commit

--- cocktail-generator/src/components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="site-footer">
-      <p>Made with <a href="https://www.thecocktaildb.com/">TheCocktailDB</a> &amp; <a href="https://developers.giphy.com/">GIPHY API</a>  &bull; HTTP5222</p>
-    </footer>
-  )
-}
-
-export default Footer
--- cocktail-generator/src/components/Header/index.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header className="site-header">
-      <a href="/" className="logo">Cocktail Generator</a>
-    </header>
-  )
-}
-
-export default Header
